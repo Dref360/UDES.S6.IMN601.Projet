ad98bfc96ace6f0c3e56d088b3a26cecb093287a
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-11-09 18:48:58 -0500

    Fix BOW and add saving for vocab

--- lib/image_util.py
+++ lib/image_util.py
@@ -1,5 +1,10 @@
+import os
+
 import cv2
 import numpy as np
+from tqdm import tqdm
+
+from lib.pickle_saving import save_obj, load_obj
 
 
 def get_BOW(datas, nbVocab=100):
@@ -9,20 +14,34 @@ def get_BOW(datas, nbVocab=100):
     :param nbVocab: Number of feature to extract
     :return:BOW vector for each dataset [[nbImage,nbVocab]]
     """
+    name = "vocab{}".format(nbVocab)
     sift = cv2.xfeatures2d.SIFT_create()
-    bowKM = cv2.BOWKMeansTrainer(nbVocab)
-    for img in np.concatenate(datas):
-        h = sift.detectAndCompute(img, None)
-        bowKM.add(np.array(h[1]))
-    vocab = bowKM.cluster()
+    if os.path.exists("obj/{}.pkl".format(name)):
+        vocab = load_obj(name)
+    else:
+        bowKM = cv2.BOWKMeansTrainer(nbVocab)
+        for img in np.concatenate(datas):
+            h = sift.detectAndCompute(img, None)
+            if h[1] is not None:
+                bowKM.add(np.array(h[1]))
+        vocab = bowKM.cluster()
+        save_obj(vocab, name)
+
     bow = cv2.BOWImgDescriptorExtractor(sift, cv2.BFMatcher(cv2.NORM_L2))
     bow.setVocabulary(vocab)
     bows = []
-    for data in datas:
+    for data in tqdm(datas):
         acc = []
         for img in data:
             key, d = sift.detectAndCompute(img, None)
             desc = bow.compute(img, key)
-            acc.append(desc)
-        bows.append(np.asarray(acc).reshape([-1, nbVocab]))
+            if desc is None:
+                acc.append(np.zeros([nbVocab]))
+            else:
+                acc.append(desc[0])
+        bows.append(np.asarray(acc))
     return bows
+
+
+def RGB_2_gray(datas):
+    return [np.asarray([cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) for im in data]).transpose([0, 1, 2]) for data in datas]
--- train.py
+++ train.py
@@ -2,11 +2,13 @@
 import argparse
 import logging
 
+import numpy as np
 from keras.datasets import mnist, cifar10
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report, precision_recall_fscore_support, accuracy_score, confusion_matrix
 
 from lib.config_reader import ConfigReader
+from lib.image_util import get_BOW, RGB_2_gray
 from src.SVM import *
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -14,6 +16,8 @@ parser.add_argument("--db", dest="db", default="mnist", type=str, help="database
 parser.add_argument("--batch_size", dest="batch_size", default=1, type=int, help="batch size")
 parser.add_argument("--n_epochs", dest="n_epochs", default=10, type=int, help="nb epochs")
 parser.add_argument("--method", dest="method", default="SVM", type=str, help="[SVM,KNN,CNN,MLP]")
+parser.add_argument("--features", dest="features", default="original", type=str, help="[original,BOW]")
+parser.add_argument("--vocab_length", dest="vocab_length", default=100, type=int, help="Length of vocabulary for BOW")
 options = parser.parse_args()
 
 logging.basicConfig(filename='logging.log', level=logging.DEBUG,
@@ -31,12 +35,22 @@ if options.db == "mnist":
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 else:
     (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    X_train = np.transpose(X_train, [0, 2, 3, 1])
+    X_test = np.transpose(X_test, [0, 2, 3, 1])
+
+assert options.features in ["original", "BOW"], "Unavailable features selections"
+if options.features == "BOW":
+    if X_train.shape[-1] != 1 and len(X_train.shape) > 3:
+        X_train, X_test = RGB_2_gray([X_train, X_test])
+    X_train, X_test = get_BOW([X_train, X_test], options.vocab_length)
+else:
+    X_train, X_test = X_train / 255., X_test / 255.
 
 X_train, X_test = classifier.preprocess([X_train, X_test])
-X_train, X_test = X_train / 255., X_test / 255.
 y_train = y_train.reshape([-1])
 y_test = y_test.reshape([-1])
-
+print("X_train shape : {}".format(X_train.shape))
+print("X_test shape : {}".format(X_test.shape))
 param_grid = classifier.get_params()
 
 print("# Tuning hyper-parameters")
