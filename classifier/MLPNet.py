b7df527ef824cd83dfdd39a7c1341b6ed91a3141
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-11-24 21:30:04 -0500

    fix mlp shaping

--- classifier/MLPNet.py
+++ classifier/MLPNet.py
@@ -1,6 +1,7 @@
 from classifier.deeplearning_classifier import BaseDeepLearning
 from keras.layers import Dense,Dropout
 from keras.models import Sequential
+from util.image_utils import linearize
 
 
 class MLPNet(BaseDeepLearning):
@@ -9,3 +10,6 @@ class MLPNet(BaseDeepLearning):
         model.add(Dense(30,activation="relu",input_shape=self.input_shape))
         model.add(Dense(self.output_size,activation="sigmoid"))
         return model
+
+    def preprocess(self, datasets):
+        return linearize(datasets)
